df29a2dda9e4a4fb05dcfcce0efb5e1a62f364f5
khasim938012 <khasim938012@gmail.com>
2026-09-16 15:11:21 +0530

    update

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,7 +1,19 @@
 import "./app.css";
 
-function app() {
-  return <h1>Hello World!</h1>;
+
+function Tittle(){
+  return <h1>I am the Tittle</h1>
 }
 
-export default app;
+function Description(){
+  return <h3>I am the Description</h3>
+}
+
+function App(){
+  return (<div>
+    <Tittle></Tittle>
+    <Description></Description>
+  </div>)
+}
+
+export default App;
